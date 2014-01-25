42ea62d2ee24149ebec4e3123b514f9c02cf9299
gamay777 <m.gammaitoni@student.isartdigital.com>
2014-01-25 02:10:44 +0100

    update main

--- js/main.js
+++ js/main.js
@@ -4,5 +4,5 @@ function run(game)
 	game.ctx.fillStyle = "#FFFFFF";
 	game.ctx.fillRect(0,0,game.canvasWidth,game.canvasHeight);
 	//On draw le player
-	game.player.draw(game.context);
+	game.player.draw(game.ctx);
 }
